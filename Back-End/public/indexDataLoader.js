d8211a37c7622004f8373352e7fe5576de3fa5d9
Hangum <hangumarchanaofficial@gmail.com>
2026-02-10 21:53:30 +0530

    updated version

--- Back-End/public/indexDataLoader.js
+++ Back-End/public/indexDataLoader.js
@@ -1,72 +1,72 @@
-let database=[];
-let product1Image =document.getElementById('HP1Image');
-let product1Name =document.getElementById('HP1Name');
-let product1Style =document.getElementById('HP1Style');
-let productId4=document.getElementById('productId4');
-let product2Image=document.getElementById('HP2Image');
-let product2Name=document.getElementById('HP2Name');
-let product2Style=document.getElementById('HP2Style');
-let productId5=document.getElementById('productId5');
-let product3Image=document.getElementById('HP3Image');
-let product3Name=document.getElementById('HP3Name');
-let product3Style=document.getElementById('HP3Style');
-let productId6=document.getElementById('productId6');
-let product4Image=document.getElementById('HP4Image');
-let product4Name=document.getElementById('HP4Name');
-let product4Style=document.getElementById('HP4Style');
-let productId7=document.getElementById('productId7');
-let product5Image=document.getElementById('HP5Image');
-let product5Name=document.getElementById('HP5Name');
-let product5Style=document.getElementById('HP5Style');
-let productId8=document.getElementById('productId8');
-let product6Image=document.getElementById('HP6Image');
-let product6Name=document.getElementById('HP6Name');
-let product6Style=  document.getElementById('HP6Style');
-let productId9=document.getElementById('productId9');
+let database = [];
+let product1Image = document.getElementById('HP1Image');
+let product1Name = document.getElementById('HP1Name');
+let product1Style = document.getElementById('HP1Style');
+let productId4 = document.getElementById('productId4');
+let product2Image = document.getElementById('HP2Image');
+let product2Name = document.getElementById('HP2Name');
+let product2Style = document.getElementById('HP2Style');
+let productId5 = document.getElementById('productId5');
+let product3Image = document.getElementById('HP3Image');
+let product3Name = document.getElementById('HP3Name');
+let product3Style = document.getElementById('HP3Style');
+let productId6 = document.getElementById('productId6');
+let product4Image = document.getElementById('HP4Image');
+let product4Name = document.getElementById('HP4Name');
+let product4Style = document.getElementById('HP4Style');
+let productId7 = document.getElementById('productId7');
+let product5Image = document.getElementById('HP5Image');
+let product5Name = document.getElementById('HP5Name');
+let product5Style = document.getElementById('HP5Style');
+let productId8 = document.getElementById('productId8');
+let product6Image = document.getElementById('HP6Image');
+let product6Name = document.getElementById('HP6Name');
+let product6Style = document.getElementById('HP6Style');
+let productId9 = document.getElementById('productId9');
 
 
-async function loadData(){
-    try{
+async function loadData() {
+    try {
         const response = await fetch('Data.json');
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error('Network response was not ok');
 
         }
-        database = await  response.json();
+        database = await response.json();
 
     }
-    catch (error){
+    catch (error) {
         console.log(error);
     }
 }
 
-async function main(){
+async function main() {
     await loadData();
     console.log(database);
-    product1Image.src = database[117].IMG;
-    product1Name.innerText = database[117].Name;
-    product1Style.innerText = database[117].Style;
-    productId4.innerText = database[117].Id;
-    product2Image.src = database[121].IMG;
-    product2Name.innerText = database[121].Name;
-    product2Style.innerText = database[121].Style;
-    productId5.innerText = database[121].Id;
-    product3Image.src = database[123].IMG;
-    product3Name.innerText = database[123].Name;
-    product3Style.innerText = database[123].Style;
-    productId6.innerText = database[123].Id;
-    product4Image.src = database[88].IMG;
-    product4Name.innerText = database[88].Name;
-    product4Style.innerText = database[88].Style;
-    productId7.innerText = database[88].Id;
-    product5Image.src = database[92].IMG;
-    product5Name.innerText = database[92].Name;
-    product5Style.innerText = database[92].Style;
-    productId8.innerText = database[92].Id;
-    product6Image.src = database[91].IMG;
-    product6Name.innerText = database[91].Name;
-    product6Style.innerText = database[91].Style;
-    productId9.innerText = database[91].Id;
+    product1Image.src = database[204].IMG;
+    product1Name.innerText = database[204].Name;
+    product1Style.innerText = database[204].Style;
+    productId4.innerText = database[204].Id;
+    product2Image.src = database[205].IMG;
+    product2Name.innerText = database[205].Name;
+    product2Style.innerText = database[205].Style;
+    productId5.innerText = database[205].Id;
+    product3Image.src = database[202].IMG;
+    product3Name.innerText = database[202].Name;
+    product3Style.innerText = database[202].Style;
+    productId6.innerText = database[202].Id;
+    product4Image.src = database[150].IMG;
+    product4Name.innerText = database[150].Name;
+    product4Style.innerText = database[150].Style;
+    productId7.innerText = database[150].Id;
+    product5Image.src = database[148].IMG;
+    product5Name.innerText = database[148].Name;
+    product5Style.innerText = database[148].Style;
+    productId8.innerText = database[148].Id;
+    product6Image.src = database[152].IMG;
+    product6Name.innerText = database[152].Name;
+    product6Style.innerText = database[152].Style;
+    productId9.innerText = database[152].Id;
 
 
 
@@ -75,7 +75,7 @@ main();
 function getRenderId(productId) {
     window.location.href = `ProductDetails.html?id=${productId}`;
 }
-function goToProduct(){
+function goToProduct() {
     window.location.href = `Product.html`;
 }
 
